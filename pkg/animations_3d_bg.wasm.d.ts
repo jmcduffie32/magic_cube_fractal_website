9c1440a2f57799744c8766236b9e7f74dd733caa
Jon McDuffie <jon@jrmcduffie.com>
2025-04-24 23:14:31 -0400

    Fix craziness

---
+++ pkg/animations_3d_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure910_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hef2d58e9a7e7a088: (a: number, b: number, c: number) => void;
+export const closure5368_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0fd9a4fd09ed25a0: (a: number, b: number) => void;
+export const closure5373_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h550bcefa1b6d9e38: (a: number, b: number) => void;
+export const closure102723_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
